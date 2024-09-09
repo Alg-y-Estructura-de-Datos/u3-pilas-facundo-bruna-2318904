cf0cbc1bdb9b4f848868d212c1dcd63092beba18
facundobruna <163002585+facundobruna@users.noreply.github.com>
2024-09-08 22:44:14 -0300

    .

---
+++ cmake-build-debug/CMakeFiles/EJ4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EJ4.dir/ej4.cpp.obj"
+  "CMakeFiles/EJ4.dir/ej4.cpp.obj.d"
+  "EJ4.exe"
+  "EJ4.exe.manifest"
+  "EJ4.pdb"
+  "libEJ4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EJ4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/practica.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practica.dir/practica.cpp.obj"
+  "CMakeFiles/practica.dir/practica.cpp.obj.d"
+  "libpractica.dll.a"
+  "practica.exe"
+  "practica.exe.manifest"
+  "practica.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
